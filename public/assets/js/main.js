cb1c6bf873e29ff4b8f911a0c847061ed1608860
Jehovanie <jehovanieram@gmail.com>
2023-05-02 14:09:05 +0100

    setting image on public

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,5 @@
+const IS_DEV_MODE= true;
+
 if(document.querySelector("#close-navleft")){
     document.querySelector("#close-navleft").addEventListener('click', () => {
         document.querySelector(".navleft").style.transform = "translateX(-50vh)"
