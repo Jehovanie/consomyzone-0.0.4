c501c6c2545b45065681b01d41dae4293b848fb1
Jehovanie <jehovanieram@gmail.com>
2023-05-31 08:58:03 +0100

    add chargement on tribut G

--- public/assets/js/account/fetchMemberTributG.js
+++ public/assets/js/account/fetchMemberTributG.js
@@ -18,6 +18,14 @@ if( document.querySelector("#fetch_member_tribug_js_jheo")){
             document.querySelector("#fetch_photo_tribug_js_jheo").classList.remove("text-white")
             document.querySelector("#fetch_photo_tribug_js_jheo").classList.add("text-primary")
         }
+
+        document.querySelector(".content_bloc_js_jheo").innerHTML = `
+            <div class="spinner-grow text-info d-block mx-auto" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        `;
+
+
         fetch("/tributG/member/list")
             .then(response => {
                 if (!response.ok) {
