b74371d2121f88d0952df31c82471ecaabd294c7
Jehovanie <jehovanieram@gmail.com>
2024-03-13 11:39:32 +0100

    Préparation environement a partir de release_v4

--- src/Entity/ProdData.php
+++ src/Entity/ProdData.php
@@ -15,8 +15,8 @@ class ProdData{
     // const  PORT_PROD= "587";
 
     const  NOM_SENDER= "CONSOMYZONE";
-    const  EMAIL_PROD= "contact@consomyzone.com";
-    const  MDP_PROD= "mN6OYgAb2";
-    const  SERVER_SMTP_PROD= "smtp-fr.securemail.pro";
-    const  PORT_PROD= "465";
+    const  EMAIL_PROD= "akra@geomadagascar.com";
+    const  MDP_PROD= "9rWtzbTQt";
+    const  SERVER_SMTP_PROD= "mail.geomadagascar.com";
+    const  PORT_PROD= "587";
 }
